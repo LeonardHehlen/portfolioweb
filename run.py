314dac3a797e78c136921dd61269bac3b76cdbc9
LeonardHehlen <leo.hehlen@gmail.com>
2020-09-26 21:54:10 +0200

    Change homepage

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from portefolio_app import app
 # ip_address = input("Hosting Ip Address ? ")
 
 if __name__ == "__main__":
-    app.run(host="192.168.1.54", debug=True)
+    app.run(host="192.168.1.14", debug=True)
